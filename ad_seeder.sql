8a5f34d0788de1e8c1f7a5f190188aa3fe23a917
emmadejong <dejong.l.emma@gmail.com>
2018-03-23 09:55:22 -0500

    worked on ad seeder file

---
+++ ad_seeder.sql
@@ -0,0 +1,10 @@
+USE el_adlister_db;
+
+INSERT INTO ads (title, description, price, user_id)
+VALUES
+  ('used bike for sale', 'slightly used bike, normal wear and tear, great for riding in the city', 80.00, 1),
+  ('box of old movies FREE', 'old box of dvds, giving away for free, please come pickup', 0.00, 2),
+  ('BRAND NEW laptop for sale', 'BRAND NEW, NEVER OPENED 2017 MacBook for sale', 1800.00, 3),
+  ('old couch for sale', 'old family couch for sale, best offer', 50.00, 3),
+  ('dining table and set of chairs', 'in great condition, large family dining table and matching set of 6 chairs', 200.00, 4),
+  ('FREE office desk', 'in okay condition, very used but still does the job', 0.00, 5);
