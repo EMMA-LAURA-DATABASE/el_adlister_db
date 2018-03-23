d75bc3b4db240f1c7dada13c8570042ffaabbd80
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-23 15:23:30 -0500

    workingoon Q/A for ADS

---
+++ answers_seeder.sql
@@ -0,0 +1,20 @@
+USE el_adlister_db;
+
+INSERT INTO ad_answers (answer, questions_id)
+  VALUE
+  ('The bike is black with neon yellow strips', 1),
+  ('X-Files, High Art, Lord of the Rings, Star Wars',2),
+  ('Yes there is Stars Wars, the first one',3),
+  ('Serra',4),
+  ('THe couch is 17 years old, smells great!',5),
+  ('You can fit 10 cats comfortably on the couch, if they all get along, you could git 37 cats on the couch, if they arent happy cats, you should probably only allow one at a time',6),
+  ('The desk is in the free category so if you want to pay me for it, thats up to you.',7),
+  ('The recliner is 3x4x5ft, weights 100lbs, holds human mass up to 500lbs',8),
+  ('I am willing to take 6k if you pay in cash',9),
+  ('The gear was used in West Virgina, virgina, maryland, and Iceland',10),
+  ('That is a great question to ask your english teacher',11),
+  ('The fins are yellow and pink stripped',12),
+  ('So, embarrassingly enough, I fell through the door drunk',13),
+  ('I special in trig, on saturdays only bc its the only day that is cornered by fun days, so its a trifecta',14),
+  ('Ironically Saturday is the trifecta day for trig so bring your angles and lets math it up',15),
+  ('Thats a great question, I honestly will go anywhere for a great doctor as I am scared of the dentist',16)
